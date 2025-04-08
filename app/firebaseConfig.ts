706408fefd40a40aa565ff37d1d5f6029db2d114
Duy Le <lehongduy.bk@gmail.com>
2025-04-08 10:33:05 +0700

    bai 1 - lab 7

---
+++ app/firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTZ8Drli00l_5le7HAvUNdCuwfCvorRLY",
+  authDomain: "cro102-reactnative.firebaseapp.com",
+  projectId: "cro102-reactnative",
+  storageBucket: "cro102-reactnative.firebasestorage.app",
+  messagingSenderId: "462759605626",
+  appId: "1:462759605626:web:d482b6586b018cc4a892c3",
+  measurementId: "G-5FZ96X072S"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
